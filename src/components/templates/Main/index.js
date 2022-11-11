6ae70da15fcd2f3256966342ef67fc0ea5f7433c
Martín Bello <marferbe@gmail.com>
2022-11-11 17:24:26 -0300

    chore: fix header on scroll

--- src/components/templates/Main/index.js
+++ src/components/templates/Main/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {node, oneOfType, arrayOf} from 'prop-types';
+import {debounce} from 'utils';
 import Header from 'UI/organisms/Header';
 import Footer from 'UI/organisms/Footer';
 import Styles from './styles';
 
 const Main = ({children, ...props}) => {
 	if (!children) return null;
+	const [dynamicPadding, setDynamicPadding] = useState(false);
+
+	const paddingScrollHandler = debounce(
+		() => (window.scrollY >= 40 ? setDynamicPadding(true) : setDynamicPadding(false)),
+		200
+	);
+
+	useEffect(() => {
+		window.addEventListener('scroll', paddingScrollHandler);
+		return () => window.removeEventListener('scroll', paddingScrollHandler);
+	}, []);
+
 	return (
-		<Styles.LayoutContainer {...props}>
+		<Styles.LayoutContainer addPadding={dynamicPadding} {...props}>
 			<Header />
 			{children}
 			<Footer />
--- src/components/templates/Main/styles.js
+++ src/components/templates/Main/styles.js
@@ -4,6 +4,7 @@ const LayoutContainer = styled.div`
 	display: block;
 	height: 100%;
 	width: 100%;
+	${({addPadding}) => addPadding && 'padding-top: 160px'};
 `;
 
 export default {LayoutContainer};
